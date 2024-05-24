d92da251176608b0acee7f48449e5566e7dd3f96
MateusMenon <134964847+MateusMenon@users.noreply.github.com>
2024-05-23 23:11:59 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -23,39 +23,45 @@ while True:
   opcao = int(input("Digite sua opção: "))
 
   if opcao == 1:
-    try:
-      x = float(input("Digite o primeiro número: "))
-      y = float(input("Digite o segundo número: "))
-      resultado = somar(x, y)
-      print(f"Soma: {resultado}")
-    except ValueError:
-      print("Entrada inválida. Digite números válidos.")
+    while True:
+      try:
+        x = float(input("Digite o primeiro número: "))
+        y = float(input("Digite o segundo número: "))
+        resultado = somar(x, y)
+        print(f"Soma: {resultado}")
+        break  # Sai do loop interno após operação bem-sucedida
+      except ValueError:
+        print("Valor inválido. Digite números válidos.")
   elif opcao == 2:
-    try:
-      x = float(input("Digite o primeiro número: "))
-      y = float(input("Digite o segundo número: "))
-      resultado = subtrair(x, y)
-      print(f"Subtração: {resultado}")
-    except ValueError:
-      print("Entrada inválida. Digite números válidos.")
+    while True:
+      try:
+        x = float(input("Digite o primeiro número: "))
+        y = float(input("Digite o segundo número: "))
+        resultado = subtrair(x, y)
+        print(f"Subtração: {resultado}")
+        break  # Sai do loop interno após operação bem-sucedida
+      except ValueError:
+        print("Valor inválido. Digite números válidos.")
   elif opcao == 3:
     try:
       x = float(input("Digite o primeiro número: "))
       y = float(input("Digite o segundo número: "))
       resultado = multiplicar(x, y)
       print(f"Multiplicação: {resultado}")
+      break  # Sai do loop interno após operação bem-sucedida
     except ValueError:
-      print("Entrada inválida. Digite números válidos.")
+        print("Valor inválido. Digite números válidos.")
   elif opcao == 4:
     try:
       x = float(input("Digite o primeiro número: "))
       y = float(input("Digite o segundo número: "))
       resultado = dividir(x, y)
       print(f"Divisão: {resultado}")
-    except ZeroDivisionError as e:
-      print(f"Erro: {e}")
+      break # Sai do loop intermo após operação bem-sucedida
     except ValueError:
-      print("Entrada inválida. Digite números válidos.")
+        print("Valor inválido. Digite números válidos.")
+  
+
   elif opcao == 5:
     print("Saindo da calculadora...")
     break
